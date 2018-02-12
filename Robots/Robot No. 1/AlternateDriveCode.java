7fa0851bf1957c4ad636d3a382522a234bda92fc
ghostlypi <parth.iyer@gmail.com>
2018-02-12 09:16:38 -0800

    Create AlternateDriveCode.java

---
+++ Robots/Robot No. 1/AlternateDriveCode.java
@@ -0,0 +1,30 @@
+/*
+		joystickRValue = joystickL.getRawAxis(3) - joystickL.getRawAxis(2);
+		joystickLValue = joystickL.getRawAxis(3) - joystickL.getRawAxis(2);
+		if(joystickL.getRawAxis(0) > 0)
+		{
+			if(joystickL.getRawAxis(3) >= joystickL.getRawAxis(2))
+			{
+				joystickLValue += joystickL.getRawAxis(0);
+				joystickRValue -= joystickL.getRawAxis(0);
+			}
+r
+			else if(joystickL.getRawAxis(3) < joystickL.getRawAxis(2))
+			{
+				joystickLValue -= joystickL.getRawAxis(0);
+				joystickRValue += joystickL.getRawAxis(0);
+			}
+		}
+		else if(joystickL.getRawAxis(0) < 0)
+		{
+			if(joystickL.getRawAxis(3) >= joystickL.getRawAxis(2))
+			{
+				joystickRValue -= joystickL.getRawAxis(0);
+				joystickLValue += joystickL.getRawAxis(0);
+			}
+			else if(joystickL.getRawAxis(3) < joystickL.getRawAxis(2))
+			{
+				joystickRValue += joystickL.getRawAxis(0);
+				joystickLValue -= joystickL.getRawAxis(0);
+			}
+		}*/
