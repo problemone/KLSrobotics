c08ae565c706b91a399d963d4f9e9d6864102c91
ghostlypi <parth.iyer@gmail.com>
2019-02-09 11:46:52 -0800

    Update Robot.java

--- Robots/Robot No. 2/Robot.java
+++ Robots/Robot No. 2/Robot.java
@@ -1,23 +1,17 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.TimedRobot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-// import edu.wpi.first.wpilibj.PWMTalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.RobotDrive;
-
-// import edu.wpi.first.wpilibj.Spark;
-//import edu.wpi.first.wpilibj.Talon;
-//import edu.wpi.first.wpilibj.CANTalon;
-//import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.TimedRobot;
-
-// import edu.wpi.first.wpilibj.Timer;
-// import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.opencv.core.Mat;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -27,73 +21,75 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * directory.
  */
 public class Robot extends TimedRobot {
-	int P, I, D = 1;
-	int integral, previous_error, setpoint = 0;
 	
-	
-	long start;
-	RobotDrive myDrive;
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	int i = 0;
-	int switchSide = 0;
-	int driverStation = 1;
+	// double integral, previous_error, setpoint = 0;
 	int count = 0;
 	double proportional = 0.3;
-	double Integral = 0.2;
+	double integral = 0.2;
 	double goal = 0;
 	double gripperMovement;
-	
 	double eMeasure;
-	double joystickLValue;
-	double joystickRValue;
-	double joystickArmValue;
 	boolean joystickGripIn;
 	boolean joystickGripOut;
-	double joystickWheelSpeedValue;
 	boolean buttonGripperIntake;
 	boolean buttonGripperRelease;
 	double error;
 	double runningSpeed = 0;
 	int integralTracker = 0;
-	double armSpeed;
 	
-	Encoder armEncoder = new Encoder(0, 1, true, Encoder.EncodingType.k4X);
+	//time
+	long start;
+	//Motors
+	RobotDrive myDrive;
+	Spark lArmSpark;
+	Spark rArmSpark;
+	TalonSRX leftTalon;
+	TalonSRX rightTalon;
 
-	TalonSRX leftTalon = new TalonSRX(0);
-	TalonSRX rightTalon = new TalonSRX(1);
-	Spark armSpark1 = new Spark(2);
-	Spark armSpark2 = new Spark(3);
+	//Encoder
+	Encoder armEncoder;
 
+	//Stored Values
+	double joystickLValue;
+	double joystickRValue;
+	double joystickArmValue;
+	double joystickWheelSpeedValue;
+
+	//Camera
+	private UsbCamera camera;
+	private CvSink sink;
+	private Mat source;
+
+	//Joysticks
 	Joystick joystick0 = new Joystick(0);
 	Joystick joystick1 = new Joystick(1);
 
-	// CANTalon leftTalon = new CANTalon(0);
-	
-	String autoSelected;
-//	SendableChooser<String> chooser = new SendableChooser<>();
-
-	// public void PID()
-	// {
-		
-	// }
-	
 	/**
 	 * This function is run when the robot is first started up and should be used
 	 * for any initialization code.
 	 */
-	
 	@Override
 	public void robotInit() {
-//		chooser.addDefault("Default Auto", defaultAuto);
-//		chooser.addObject("My Auto", customAuto);
-//		SmartDashboard.putData("Auto choices", chooser);
+		//Drive Train
 		myDrive = new RobotDrive(0, 1);
+		//Arm
+		lArmSpark = new Spark(2);
+		rArmSpark = new Spark(3);
+		//Intake
+		leftTalon = new TalonSRX(0);
+		rightTalon = new TalonSRX(1);
+		//encoder stuff
+		armEncoder  = new Encoder(0, 1, true, Encoder.EncodingType.k4X);
 		armEncoder.setMaxPeriod(0.05);
 		armEncoder.setMinRate(10);
-		armEncoder.setDistancePerPulse(2.8125);
+		armEncoder.setDistancePerPulse(2.8125 * 15/42);
 		armEncoder.setSamplesToAverage(10);
 		armEncoder.reset();
+		//Camera
+		// camera = CameraServer.getInstance().startAutomaticCapture();
+		// camera.setResolution(840,680);
+		// sink = CameraServer.getInstance().getVideo();
+		// source = new Mat();
 	}
  
 	/**
@@ -109,13 +105,11 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-//		autoSelected = chooser.getSelected();	System.out.println("Auto selected: " + autoSelected);
-		// start = System.currentTimeMillis();
+		start = System.currentTimeMillis();
 	}
 	/**
 	 * This function is called periodically during autonomous
 	 */
-	// @SuppressWarnings("deprecation")
 	@Override
 	
 	public void autonomousPeriodic() {
@@ -124,63 +118,73 @@ public class Robot extends TimedRobot {
 	/**
 	 * This function is called periodically during operator control
 	 */
+
+	
+
 	@Override
 	public void teleopPeriodic() {
-		if(joystick0.getRawButton(1))
+		//Arm Movement
+		if(joystick1.getRawButton(1))
 		{
-			goal = 1.0;
+			goal = 30;
 			integralTracker = 0;
 		}
-		else if(joystick0.getRawButton(2))
+		else if(joystick1.getRawButton(2))
 		{
-			goal = 0.0;
+			goal = 0;
 			integralTracker = 0;
 		}
-		else if(joystick0.getRawButton(3))
+		else if(joystick1.getRawButton(3))
 		{
-			goal = 0.5;
-			integralTracker = 0;
+			goal = 15;
+			integralTracker = 0; 
 		}
-
-		armSpeed = joystick0.getRawAxis(1);
-		armSpark1.set(armSpeed);
-		armSpark2.set(armSpeed);
-
+		else if(joystick1.getRawButton(12)){
+			goal = -2;
+		}
+		goal = -2;
+		
+		// //Encoder
 		count = armEncoder.get();
 		eMeasure = armEncoder.getDistance();
-		System.out.println(eMeasure);
-		error = (360 * goal) - eMeasure;
+		//System.out.println(eMeasure);
+		error = goal - eMeasure;
 		if(count % 5 == 0)
 		{
 			integralTracker += error;
 		}
-		runningSpeed = ((error * proportional)/360) + ((integralTracker * Integral)/360);
-		
-		// leftTalon.set(runningSpeed);
-		// rightTalon.set(-runningSpeed);
-		
-		joystickLValue = joystick0.getRawAxis(1);
-		joystickRValue = joystick0.getRawAxis(1)*0.913;
-		
-//		joystickArmValue = -joystick1.getRawAxis(1);
-//		joystickGripIn = joystick1.getRawButton(0);
-//		joystickGripOut = joystick1.getRawButton(1);
-		gripperMovement = joystick1.getRawAxis(2) - joystick1.getRawAxis(3);
-		leftTalon.set(ControlMode.PercentOutput, gripperMovement);
-		rightTalon.set(ControlMode.PercentOutput, -gripperMovement);
-		
-		// For Calibration of sides
-    	if(joystick0.getRawAxis(2) < -0.1)
-    	{
-    		joystickLValue -= joystick0.getRawAxis(2);
-    		joystickRValue += joystick0.getRawAxis(2);
-    	}
-    	if(joystick0.getRawAxis(2) > 0.1)
-    	{
-    		joystickLValue -= joystick0.getRawAxis(2);
-    		joystickRValue += joystick0.getRawAxis(2);
-    	}
+		runningSpeed = ((error * proportional)/360) + ((integralTracker * integral)/360);
+
+		//Arm
+		joystickArmValue = -joystick1.getRawAxis(1);
+		if(goal != -2)
+		{
+			joystickArmValue = -runningSpeed;
+		}
+		lArmSpark.set(joystickArmValue);
+		rArmSpark.set(joystickArmValue);
+
+		//intake
+		if(joystick1.getRawButton(1)){
+			joystickWheelSpeedValue = 0.85;
+		}else if(joystick1.getRawButton(2)){
+			joystickWheelSpeedValue = -0.85;
+		}else{
+			joystickWheelSpeedValue = -0.2;
+		}
+
+		leftTalon.set(ControlMode.PercentOutput,-joystickWheelSpeedValue);
+		rightTalon.set(ControlMode.PercentOutput,joystickWheelSpeedValue);
+
+		//Drive Train
+		joystickLValue = -joystick0.getRawAxis(1) + joystick0.getRawAxis(2);
+		joystickRValue = -joystick0.getRawAxis(1) - joystick0.getRawAxis(2);
 		myDrive.tankDrive(joystickLValue, joystickRValue);
+		
+		//Camera
+		// sink.grabFrame(source);
+		// MyVector choice = ProcessImage.processImage(source);
+		// return choice.getAngle();
 	}
 
 	/**
